dfaf355693b3a202b2687227b912993c9db6685d
Ashi Krishnan <me@ashi.works>
2016-05-17 15:45:33 -0400

    Resize canvas when the window resizes (if needed), and save and restore the image content.

--- browser/whiteboard.js
+++ browser/whiteboard.js
@@ -25,21 +25,46 @@ window.whiteboard = new window.EventEmitter();
 
     });
 
-    var canvas = document.querySelector('#paint');
-    var sketch = document.querySelector('#sketch');
-    var sketchStyle = getComputedStyle(sketch);
+    var canvas = document.getElementById('paint');
+    
+    var ctx = canvas.getContext('2d')
 
-    var pixelRatio = window.devicePixelRatio || 1;
-    canvas.width = parseInt(sketchStyle.getPropertyValue('width')) * pixelRatio;
-    canvas.height = parseInt(sketchStyle.getPropertyValue('height')) * pixelRatio;
+    function resize() {
+        // Unscale the canvas (if it was previously scaled)
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        
+        // The device pixel ratio is the multiplier between CSS pixels
+        // and device pixels
+        var pixelRatio = window.devicePixelRatio || 1;    
+        
+        // Allocate backing store large enough to give us a 1:1 device pixel
+        // to canvas pixel ratio.
+        var w = canvas.clientWidth * pixelRatio,
+            h = canvas.clientHeight * pixelRatio;
+        if (w !== canvas.width || h !== canvas.height) {
+            // Resizing the canvas destroys the current content.
+            // So, save it...
+            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
 
-    var ctx = canvas.getContext('2d');
-    ctx.scale(pixelRatio, pixelRatio);
+            canvas.width = w; canvas.height = h;
 
-    ctx.lineWidth = 5;
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
+            // ...then restore it.
+            ctx.putImageData(imgData, 0, 0)
+        }
 
+        // Scale the canvas' internal coordinate system by the device pixel
+        // ratio to ensure that 1 canvas unit = 1 css pixel, even though our
+        // backing store is larger.
+        ctx.scale(pixelRatio, pixelRatio);
+        
+        ctx.lineWidth = 5
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';     
+    }
+    
+    resize()
+    window.addEventListener('resize', resize) 
+    
     var currentMousePosition = {
         x: 0,
         y: 0
