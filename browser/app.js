b5d6de2b074ad7f1da7c0f508e4e6f3836200d3f
Ashi Krishnan <me@ashi.works>
2017-10-04 12:07:51 -0400

    Better comments.

--- browser/app.js
+++ browser/app.js
@@ -1,10 +1,11 @@
 // Import from the module './whiteboard':
 //   The default export, naming it draw,
-//   An export named 'events', calling it 'whiteboard'.
+//   An export named `events`, calling it `whiteboard`.
 import draw, {events as whiteboard} from './whiteboard'
 
-// This logging will probably get really annoying:
+// Example of listening to draw events:
+//   (This logging will probably get really annoying):
 whiteboard.on('draw', console.log)
 
-// Draw a single stroke.
+// Example: Draw a single stroke.
 draw([0, 0], [250, 250], 'red', true)
