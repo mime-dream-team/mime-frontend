eda3834ea3928d0ca25c4f6532902517e340fb34
Emily Intersimone <emily.intersimone@gmail.com>
2017-10-05 12:57:52 -0400

    working solution without extra credit

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const socketio = require('socket.io');
 
 // app.listen() returns an http.Server object
 // http://expressjs.com/en/4x/api.html#app.listen
 const server = app.listen(1337, function () {
     console.log(`Listening on http://localhost:${server.address().port}`);
+});
+
+const io = socketio(server);
+
+const inMemoryDrawHistory = [];
+
+io.on('connection', socket => {
+  console.log('A new client has connected!');
+  console.log(socket.id);
+
+  if (inMemoryDrawHistory.length) socket.emit('load', inMemoryDrawHistory);
+
+  socket.on('draw', function (start, end, color) {
+    inMemoryDrawHistory.push({ start, end, color });
+    socket.broadcast.emit('draw', start, end, color);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('Goodbye, ', socket.id, ' :(');
+  });
+});
+
+io.on('disconnect', socket => {
+  console.log(`Socket #${socket.id} has disconnected :(`)
 });
 
 // The webpack dev middleware builds and serves our bundle
