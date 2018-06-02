04884129221795d7339862577797172bc6713cee
Lizz Thabet <lizzthabet@gmail.com>
2018-06-02 12:46:20 -0400

    add `react-redux` to app

--- browser/app.js
+++ browser/app.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 
 ReactDOM.render(
-	<div>Hello, world!</div>,
+	<Provider store = { store }>
+		<div>Hello, world!</div>
+	</Provider>,
 	document.getElementById('app')
 )
