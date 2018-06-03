0c2f640d1af413c75148968501e9d5f73b80f720
Lizz Thabet <lizzthabet@gmail.com>
2018-06-03 15:53:23 -0400

    another console log

--- server/auth/index.js
+++ server/auth/index.js
@@ -8,7 +8,6 @@ router.get('/me', (req, res, next) => {
 router.put('/login', (req, res, next) => {
 	User.findOne({ where: { email: req.body.email } })
 		.then(user => {
-			console.log(user)
 			if (!user) res.status(401).send('User not found')
 			else if (!user.correctPassword(req.body.password)) res.status(401).send('Incorrect password')
 			else req.login(user, error => {
