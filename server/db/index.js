37f33df3bfdc9541796985afe63cf0c3f2b76efb
Lizz Thabet <lizzthabet@gmail.com>
2018-06-03 14:47:23 -0400

    add salt, encrypt, and password methods to User model

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = require('./_db')
-const User = require('../models/User')
+const User = require('./models/User')
 
 // import all models here
 // define all relationships here
--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,8 +1,40 @@
 const Sequelize = require('sequelize')
+const crypto = require('crypto')
+const _ = require('lodash')
 const db = require('../_db')
 
 const User = db.define('user', {
-	name: Sequelize.STRING
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			isEmail: true
+		}
+	},
+	password: Sequelize.STRING,
+	salt: Sequelize.STRING
 })
+
+const setSaltAndPassword = user => {
+	if (user.changed('password')) {
+		user.salt = User.generateSalt()
+		user.password = User.encryptPassword(user.password, user.salt)
+	}
+}
+
+User.addHook('beforeCreate', 'beforeUpdate', setSaltAndPassword)
+
+User.generateSalt = () => crypto.randomBytes(16).toString('base64')
+
+User.encryptPassword = (plainTxt, salt) => {
+	const hash = crypto.createHash('sha1')
+	hash.update(plainTxt)
+	hash.update(salt)
+	return hash.digest('hex')
+}
+
+User.prototype.correctPassword = password => this.Model.encryptPassword(password, this.salt) === this.password
+
+User.prototype.sanitize = () => _.omit(this.toJSON(), ['password', 'salt'])
 
 module.exports = User
