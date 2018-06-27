08dc76826a03bf892da95bce1eeec8fa07f45a60
Lizz Thabet <lizzthabet@gmail.com>
2018-06-27 14:30:54 -0400

    refactor(index): rename app entry point to `index.js` and add a `Root` component that will be the main wrapper component for the app

---
+++ browser/Root.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Routes from './routes/Routes'
+import Whiteboard from './components/Whiteboard'
+
+const Root = () => {
+	return (
+		<section>
+			<Routes />
+		</section>
+	)
+}
+
+export default Root
--- browser/index.js
+++ browser/index.js
@@ -1,19 +1,16 @@
-import Whiteboard from './Whiteboard'
-
-// React / Redux setup
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
 import history from './routes/history'
 import store from './store/store'
+import Root from './Root'
 import './styles/main.css'
 
 ReactDOM.render(
 	<Provider store = { store }>
 		<Router history={ history }>
-			{/*This will be our upper-most app wrapper*/}
-			{/*In the app component, we'll require in the routes component*/}
+			<Root />
 		</Router>
 	</Provider>,
 	document.getElementById('app')
