c14206fc0813fd5a73e7e12b39b0007c0fd8acb1
Lizz Thabet <lizzthabet@gmail.com>
2018-06-28 12:33:23 -0400

    refactor(whiteboard): update konva `Image` component to follow React conventions with `refs` and make `createCanvas` functions

--- browser/components/Whiteboard.js
+++ browser/components/Whiteboard.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 import { Stage, Layer, Image } from 'react-konva'
 
+// These dimensions control the size of the canvas and Image component that forms the drawing surface
+const drawingHeight = window.innerHeight - 25
+const drawingWidth = window.innerWidth - 25
+
 export default class Whiteboard extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
 			isDrawing: false,
-			strokePool: []
+			strokePool: [],
+			canvas: '',
+			context: ''
 		}
+		this.stage = React.createRef()
 		this.image = React.createRef()
 	}
 
 	componentDidMount() {
+		this.createCanvas()
+	}
+
+	// Note: the canvas needs to be the same size as the Image component
+	createCanvas = () => {
 		const canvas = document.createElement('canvas')
-		canvas.width = 500
-		canvas.height = 500
+		canvas.width = drawingWidth
+		canvas.height = drawingHeight
 		const context = canvas.getContext('2d')
 		this.setState({ canvas, context })
 	}
@@ -32,10 +44,9 @@ export default class Whiteboard extends Component {
 	}
 
 	handleMouseMove = () => {
-		console.log(this.state.strokePool)
-		const { context, isDrawing } = this.state
-
+		const { isDrawing, context } = this.state
 		if (isDrawing) {
+			console.log(this.state.strokePool)
 			context.strokeStyle = '#000000'
 			context.lineJoin = 'round'
 			context.lineWidth = 5
@@ -66,22 +77,24 @@ export default class Whiteboard extends Component {
 
 	render() {
 		const { canvas } = this.state
-
 		return (
-			<Stage width={700} height={700}>
-				<Layer>
-					<Image
-						image={canvas}
-						ref={this.image}
-						width={500}
-						height={500}
-						stroke='black'
-						onMouseDown={this.handleMouseDown}
-						onMouseUp={this.handleMouseUp}
-						onMouseMove={this.handleMouseMove}
-					/>
-				</Layer>
-			</Stage>
+			<section>
+				<Stage width={window.innerWidth} height={window.innerHeight} ref={this.stage}>
+					<Layer>
+						<Image
+							image={canvas}
+							ref={this.image}
+							width={drawingWidth}
+							height={drawingHeight}
+							stroke='black'
+							onMouseDown={this.handleMouseDown}
+							onMouseUp={this.handleMouseUp}
+							onMouseMove={this.handleMouseMove}
+						/>
+					</Layer>
+					{/* All wireframe shapes will be in a new layer */}
+				</Stage>
+			</section>
 		)
 	}
 }
