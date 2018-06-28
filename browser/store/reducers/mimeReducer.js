12e94b124bc32f5382a11703952411cd76d66628
Lizz Thabet <lizzthabet@gmail.com>
2018-06-28 17:34:43 -0400

    feat(mimeReducer): create new state for reducer and `add new shape` action

--- browser/store/reducers/mimeReducer.js
+++ browser/store/reducers/mimeReducer.js
@@ -1,34 +1,19 @@
 import axios from 'axios'
 
-// action types
+// Action types
 const ADD_NEW_SHAPE = 'ADD_NEW_SHAPE'
 
-// action creators
+// Action creators
 const addNewShape = interpretedShape => {
 	return { type: ADD_NEW_SHAPE, interpretedShape }
 }
 
-// thunks
-// wrap async code
-const actionCreatorThunk = data => dispatch => {
-	axios
-		.get('/api/user/:data')
-		.then(newData => dispatch(actionCreator(newData.data)))
-		.catch(console.error)
-}
-
-// initial state
-// each piece of state should be typed
-const initialState = {
-	mimeObjects: []
-}
-
-// reducer
-// must return a new state and not mutate previous state
-const reducer = (state = initialState, action) => {
+// Reducer
+// Mime state will be an array of objects, where objects contain shape data
+const reducer = (state = [], action) => {
 	switch (action.type) {
-	case ACTION_TYPE:
-		return { data: action.data }
+	case ADD_NEW_SHAPE:
+		return [ ...state, action.interpretedShape ]
 	default:
 		return state
 	}
