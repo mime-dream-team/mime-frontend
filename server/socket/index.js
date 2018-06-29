513c8bc17d66b378d9d156bbfc878180dd204420
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro-2.local>
2018-06-29 16:43:25 -0400

    feat(socket): add shape prediction socket functionality with shape dimensions to the frontend

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,3 +1,6 @@
+const getPrediction = require('./../model')
+const processTrainingData = require('./../utils/dataTransforms/processTrainingData')
+const shapeCreator = require('./../utils/shapeCreator')
 module.exports = io => {
 	io.on('connection', socket => {
 		console.log(socket.id, ' has made a persistent connection to the server!')
@@ -7,12 +10,16 @@ module.exports = io => {
 			// To do: send the strokePool to model for shape detection
 			// To do: process strokePool with the `utils`
 			// To do: emit the interpreted shape object
-
 			// Note: these parameters are hardcoded for testing ONLY
-			socket.emit('addNewShape', { key: i, x, y, radius })
-			i++
-			x = x + 50
-			y = y + 35
+			let processedStroke = processTrainingData([ {stroke: [ strokePool ], type: 'unknown'} ]).shapeTrainingDataPoints[0]
+			getPrediction(processedStroke)
+				.then(shape => {
+					const [circle, square] = shape
+					const shapeData = shapeCreator(strokePool, circle > square ? 'circle' : 'square')
+					socket.emit('addNewShape', shapeData)
+					console.log(shapeData)
+					console.log(circle, square)
+				})
 		})
 
 	})
