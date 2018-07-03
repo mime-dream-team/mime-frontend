da5ace019a6a80c3203382f751c4470cea69db5e
Lizz Thabet <lizzthabet@gmail.com>
2018-07-03 16:43:19 -0400

    feat(MimeView): point routes and `Home` and `Root` component to `MimeView`

--- browser/components/Home.js
+++ browser/components/Home.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import MakeMime from './MakeMime'
-
+import { Container, Row, Col } from 'reactstrap'
 
 const Home = () => {
 	return (
-		<section>
-			<h1>Welcome to MIME</h1>
-			<p>Mime is a simple, easy-to-use wireframing tool. It uses a drawing assistant AI to transform your hand-drawn sketches to clean rectangles, circles, and lines in real-time. Like magic! Get started by entering the size of your wireframe.</p>
-			<MakeMime />
-		</section>
+		<Container fluid={true}>
+			<Row>
+				<Col xs={{ size: '12' }} sm={{ size: '6', offset: '3' }}>
+					<header className='home__header'>
+						<h1 className='home__nameplate'>MIME</h1>
+					</header>
+					<h1>&#9634; &#9651; &#9644;</h1>
+					<p className='home__intro'><span className='underline'>Mime</span> is a simple, easy-to-use wireframing tool. It uses a drawing assistant AI to transform your hand-drawn sketches into clean rectangles, circles, and lines in real-time. Like magic! Get started by entering the size of your wireframe.</p>
+					<MakeMime />
+				</Col>
+			</Row>
+		</Container>
 	)
 }
-
 
 export default Home
--- browser/routes/routes.js
+++ browser/routes/routes.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Mime from '../components/Mime'
+import MimeView from '../components/MimeView'
 import Home from '../components/Home'
 
 const Routes = () => {
 	return (
 		<Switch>
 			<Route exact path='/' component={ Home } />
-			<Route exact path='/mime/:urlId' component={ Mime } />
+			<Route exact path='/mime/:urlId' component={ MimeView } />
 			<Route component={ Home } />
 		</Switch>
 	)
