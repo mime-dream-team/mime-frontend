24c23d448a7c2d2a94a8f3a2280137765964f37f
Lizz Thabet <lizzthabet@gmail.com>
2018-07-03 16:44:01 -0400

    feat(Nav): build basic nav using reactstrap and bootstrap

---
+++ browser/components/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
+import Share from './Share'
+
+const Navigation = (props) => {
+
+	return (
+		<Navbar fixed='true'>
+			<NavbarBrand href='/'>MIME</NavbarBrand>
+			<Share path={props.match.path} />
+		</Navbar>
+	)
+}
+
+const mapStateToProps = (state) => {
+	return state.lastSave
+}
+
+export default connect(
+	mapStateToProps
+)(Navigation)
