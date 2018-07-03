995c341e306ad20061821f1632561c70e38d87e9
Chiara Marcial Martínez <cmarmar@gmail.com>
2018-07-03 14:48:17 -0400

    refactor(MakeMime): remove old buttons

--- browser/components/MakeMime.js
+++ browser/components/MakeMime.js
@@ -3,6 +3,14 @@ import { connect } from 'react-redux'
 import { createMimeThunk } from '../store/reducers/mimeReducer'
 
 class MakeMime extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			height: 0,
+			width: 0
+		}
+		this.handleMimeCreate = this.handleMimeCreate.bind(this)
+	}
 	handleMimeCreate(height, width) {
 		const dimensionsObj = { height, width }
 		this.props.createMimeThunk(dimensionsObj, this.props.history)
@@ -10,31 +18,9 @@ class MakeMime extends Component {
 	render() {
 		return (
 			<div>
-				<div>
-					<h1>
-						Select a size to create your mime wireframe.
-					</h1>
-				</div>
-				<div>
-					<button
-						type='button'
-						onClick={() => this.handleMimeCreate(768, 1024)}
-					>
-						768 x 1024
-					</button>
-					<button
-						type='button'
-						onClick={() => this.handleMimeCreate(1920, 1080)}
-					>
-						1920 x 1080
-					</button>
-					<button
-						type='button'
-						onClick={() => this.handleMimeCreate(2560, 1440)}
-					>
-						2560 x 1440
-					</button>
-				</div>
+				<input name={height} />
+				<input name={width} />
+				<button />
 			</div>
 		)
 	}
