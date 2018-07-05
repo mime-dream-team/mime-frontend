971706cefb8b034ae9f192c593c3d5efe0d9badc
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro-2.local>
2018-07-05 11:00:08 -0400

    feat(Home): add SVG MIME logo to the homepage with animations

--- browser/components/Home.js
+++ browser/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import MakeMime from './MakeMime'
 import { Container, Row, Col } from 'reactstrap'
+import Logo from './Logo';
 
 const Home = (props) => {
 	return (
@@ -8,9 +9,8 @@ const Home = (props) => {
 			<Row>
 				<Col sm={{ size: '10', offset: '1' }} md={{ size: '6', offset: '3' }}>
 					<header className='home__header'>
-						<h1 className='home__nameplate'>MIME</h1>
+						<Logo />
 					</header>
-					<h1>&#9634; &#9651; &#9644;</h1>
 					<p className='home__intro'><span className='underline'>Mime</span> is a simple, easy-to-use wireframing tool. It uses a drawing assistant AI to transform your hand-drawn sketches into clean rectangles, circles, and lines in real-time. Like magic!</p>
 					<p className='home__intro'>Get started by entering the size of your wireframe.</p>
 					<MakeMime history={ props.history } />
