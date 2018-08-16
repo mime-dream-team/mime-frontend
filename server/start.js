ddd044ec1976496dfbd284f20aba72da240efec1
Lizz Thabet <lizzthabet@gmail.com>
2018-08-16 15:51:51 -0400

    remove devDependencies in server start and force commit bundlejs

--- server/start.js
+++ server/start.js
@@ -2,12 +2,11 @@
 const socketio = require('socket.io')
 const db = require('./db/_db')
 const app = require('./server')
-const chalk = require('chalk')
 const port = process.env.PORT || 1337
 
 db.sync().then(() => {
 	const server = app.listen(port, () => {
-		console.log(chalk.inverse(`server up and running on port ${port}`))
+		console.log(`server up and running on port ${port}`)
 		const io = socketio(server) // set up socket.io to communicate between frontend & server
 		require('./socket')(io)
 	})
